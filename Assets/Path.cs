314d1fc5a04a9f8883e1e145b333c97efbfb0e8d
fedebamba <federicobambagioni@gmail.com>
2018-03-24 15:25:01 +0100

    commit2

---
+++ Assets/Path.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+    //public LinkedList<Drop> drops;
+    public GameObject template;
+
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+        
+
+
+}
